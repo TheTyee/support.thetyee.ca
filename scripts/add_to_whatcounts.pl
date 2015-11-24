a950458887a6582261fcb618f929ac354d080051
Bryan Carney <bcarney@ifex.org>
2015-11-23 19:08:42 -0500

    add_to_whatcounts fixed template id

--- scripts/add_to_whatcounts.pl
+++ scripts/add_to_whatcounts.pl
@@ -268,7 +268,7 @@ sub _send_message {
         to          => $record->email,
         from        => '"The Tyee" <builders@thetyee.ca>',
         charset     => 'ISO-8859-1',
-        template_id => '955',
+        template_id => '1190',
         data        => "amount,plan_code,hosted_login_token^$amount,$plan_code,$hosted_login_token"
     };
 
