51c07be8122142d4fb2c563e32e0c474d16505fe
Phillip Smith <ps@phillipadsmith.com>
2016-11-18 12:44:02 -0800

    Add columns for new data

--- sqitch/revert/transactions.sql
+++ sqitch/revert/transactions.sql
@@ -5,6 +5,13 @@ BEGIN;
 
     SET client_min_messages = 'warning';
     
-    ALTER TABLE support.transactions DROP COLUMN pref_lapel TEXT NULL;
+    ALTER TABLE support.transactions DROP COLUMN payment_type TEXT NULL;
+    ALTER TABLE support.transactions DROP COLUMN transit_number TEXT NULL;
+    ALTER TABLE support.transactions DROP COLUMN bank_number TEXT NULL;
+    ALTER TABLE support.transactions DROP COLUMN account_number TEXT NULL;
+    ALTER TABLE support.transactions DROP COLUMN person TEXT NULL;
+    ALTER TABLE support.transactions DROP COLUMN referrer TEXT NULL;
+    ALTER TABLE support.transactions DROP COLUMN on_behalf_of TEXT NULL;
+    ALTER TABLE support.transactions DROP COLUMN on_behalf_of_name TEXT NULL;
 
 COMMIT;
