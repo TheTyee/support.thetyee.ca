c7952bdbfd2834f2ae581e281d0df83f453d1a5b
Phillip Smith <ps@phillipadsmith.com>
2016-11-21 11:01:29 -0800

    Add column for address1

--- sqitch/revert/transactions.sql
+++ sqitch/revert/transactions.sql
@@ -5,15 +5,6 @@ BEGIN;
 
     SET client_min_messages = 'warning';
     
-    ALTER TABLE support.transactions DROP COLUMN payment_type;
-    ALTER TABLE support.transactions DROP COLUMN phone;
-    ALTER TABLE support.transactions DROP COLUMN transit_number;
-    ALTER TABLE support.transactions DROP COLUMN bank_number;
-    ALTER TABLE support.transactions DROP COLUMN account_number;
-    ALTER TABLE support.transactions DROP COLUMN raiser;
-    ALTER TABLE support.transactions DROP COLUMN referrer;
-    ALTER TABLE support.transactions DROP COLUMN on_behalf_of;
-    ALTER TABLE support.transactions DROP COLUMN on_behalf_of_name_first;
-    ALTER TABLE support.transactions DROP COLUMN on_behalf_of_name_last;
+    ALTER TABLE support.transactions DROP COLUMN address1;
 
 COMMIT;
---
+++ sqitch/revert/transactions@v1.7.2.sql
@@ -0,0 +1,19 @@
+-- Deploy transactions
+-- requires: schema
+
+BEGIN;
+
+    SET client_min_messages = 'warning';
+    
+    ALTER TABLE support.transactions DROP COLUMN payment_type;
+    ALTER TABLE support.transactions DROP COLUMN phone;
+    ALTER TABLE support.transactions DROP COLUMN transit_number;
+    ALTER TABLE support.transactions DROP COLUMN bank_number;
+    ALTER TABLE support.transactions DROP COLUMN account_number;
+    ALTER TABLE support.transactions DROP COLUMN raiser;
+    ALTER TABLE support.transactions DROP COLUMN referrer;
+    ALTER TABLE support.transactions DROP COLUMN on_behalf_of;
+    ALTER TABLE support.transactions DROP COLUMN on_behalf_of_name_first;
+    ALTER TABLE support.transactions DROP COLUMN on_behalf_of_name_last;
+
+COMMIT;
