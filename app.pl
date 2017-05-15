6317a350059afcbe0d50f927235d9b100de868a4
api@thetyee.ca <tyeeapps@tyeeweb.nmsrv.com>
2017-05-15 13:27:22 -0700

    data-recurly on country change fix

--- app.pl
+++ app.pl
@@ -198,8 +198,8 @@ group {
     any [qw(GET POST)] => '/' => sub {
         my $self = shift;
         $self->stash( body_id => 'evergreen', );
-        $self->flash( appeal_code => 'election2017-2' );
-    } => 'election2017-2';
+        $self->flash( appeal_code => 'evergreen' );
+    } => 'evergreen';
 
     any [qw(GET POST)] => '/powermap' => sub {
         my $self = shift;
